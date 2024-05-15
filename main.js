cb984a1ee6e52aecdf2add2eea7e779a875d548f
AshishPanda7777 <pandaashish910@gmail.com>
2024-05-15 19:10:41 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var typed = new Typed(".text",{
+    strings:["Python Developer", "Web Developer","Django Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
+
+
